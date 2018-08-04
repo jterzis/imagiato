0ce549722c4a12bf09a2dbc272ab6229b2367605
John Terzis <john.c.terzis@gmail.com>
2018-08-04 16:49:25 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from './utils/getWeb3'
 
